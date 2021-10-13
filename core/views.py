9c73af19d1f635b8e477424cc39a046f7a7a7ce2
Ryan Pereira <ryan_pereira_92@hotmail.com>
2021-10-13 10:07:44 +0530

    Added Signup Page with Signup Form

--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
+from .forms import SignUpForm
 
 # Create your views here.
 
@@ -8,11 +10,32 @@ def home(request):
     return render(request, 'home.html')
 
 
-@login_required
+@login_required()
 def customer_page(request):
     return render(request, 'home.html')
 
 
-@login_required
+@login_required()
 def courier_page(request):
     return render(request, 'home.html')
+
+
+def sign_up(request):
+    form = SignUpForm()
+
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+
+        if form.is_valid():
+            email = form.cleaned_data.get('email').lower()
+
+            user = form.save(commit=False)
+            user.username = email
+
+            user.save()
+
+            login(request, user)
+            return redirect('/')
+
+    context = {'form': form}
+    return render(request, 'sign_up.html', context)
--- djangopwa/urls.py
+++ djangopwa/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
 
     path('sign-in/', auth_views.LoginView.as_view(template_name="sign_in.html")),
     path('sign-out/', auth_views.LogoutView.as_view(next_page="/")),
+    path('sign-up/', views.sign_up),
 ]
